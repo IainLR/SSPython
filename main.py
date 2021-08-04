c35449a2af2511097c26890a9ad7ea623621c4e1
IainLR <iainrobertzon@gmail.com>
2021-08-04 13:50:43 -0500

    python assignment 1

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+
+items = json.loads('[{"id":1, "text": "Item 1"}, {"id":2, "text": "Item 2"}]')
+
+for item in items:
+    print(item['text'])
+
+# 1
+print(50+50)
+print(100-10)
+
+# 2
+# print(30+*6,6^6,6**6,6+6+6+6+6+6)
+print(30 +6)
+print(pow(6, 6))
+print(6+6+6+6+6+6)
+
+# 3
+print("Hello World")
+
+for i in range(10):
+    print("Hello World : " + str(i+1))
+
+# 4
+
+def monthly_payment_calc(loanSize, interestRate, lengthOT): 
+    monthlyRate = interestRate/12
+    payment = (loanSize * monthlyRate) / (1 - pow((1 + monthlyRate), -lengthOT))
+
+    print(round(payment))
+
+monthly_payment_calc(800000, .06, 103)
