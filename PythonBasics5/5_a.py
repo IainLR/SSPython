3c845ca5a43a4d21b7ba2000c4a5ae3ae0fc8e32
IainLR <iainrobertzon@gmail.com>
2021-08-06 17:07:56 -0500

    bmi

--- PythonBasics5/5_a.py
+++ PythonBasics5/5_a.py
@@ -3,10 +3,10 @@ def BMI(weight, height):
 
 print(BMI(80, 1.73))
 
-def bmi_checker(num_of_people, list_of_stats):
+def bmi_checker(list_of_stats):
     res_string = ''
     for item in list_of_stats:
-        print(item)
+        # print(item)
         this_bmi = BMI(item[0], item[1])
         if this_bmi < 18.5:
             res_string += 'under '
@@ -20,4 +20,14 @@ def bmi_checker(num_of_people, list_of_stats):
 
 stat_list = [[80, 1.73], [55, 1.58], [49, 1.91]]
 
-print(bmi_checker(3, stat_list))
+x = input("how many sets of data are entering today?\n")
+x_int = int(x)
+the_list = []
+for item in range(x_int):
+    weight = int(input("weight: "))
+    height = float(input("height: "))
+    the_list.append(list([weight, height]))
+# print(the_list)
+
+
+print(bmi_checker(the_list))  
